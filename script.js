d59b6fa90e872fe0130b992f34111bd9f6871b9a
huangmelanie03 <102622167+huangmelanie03@users.noreply.github.com>
2023-12-15 14:46:32 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Yaayayay!!!";
+    gif.src = "https://i.pinimg.com/originals/22/ae/ee/22aeee48c538e424221a1e29f3c9a221.gif";
+})
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+})
